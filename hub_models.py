70cdce2bb8f52a9af7570b60a8e1cf8fb0b75bcf
Ashwin-Ramesh2607 <ashwin.cuber26@gmail.com>
2020-04-08 19:24:29 +0530

    added support for multiple tf hub models

--- hub_models.py
+++ hub_models.py
@@ -4,12 +4,9 @@ import tensorflow_hub as hub
 
 def get_hub_url(architecture):
 
-    url_list = {
-        'mobilenet_v2_100_224': 'https://tfhub.dev/google/imagenet/mobilenet_v2_100_224/feature_vector/4',
-        'inception_v3': 'https://tfhub.dev/google/imagenet/inception_v3/feature_vector/4'
-    }
+    hub_url = f'https://tfhub.dev/google/imagenet/{architecture}/feature_vector/4'
 
-    return url_list[architecture]
+    return hub_url
 
 
 def get_input_image_size(module_layer):
@@ -42,4 +39,5 @@ def get_model_shapes(architecture):
 def get_hub_model(architecture):
 
     hub_url = get_hub_url(architecture)
+    
     return hub.load(hub_url)
